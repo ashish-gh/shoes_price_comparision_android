e0ccde255ece1b233ecb3c7f43ca68fdc80aa459
ashish ghimire <ashish.gh123@gmail.com>
2019-04-22 17:55:13 +0545

    Initial design of Bottom navigation

--- app/src/main/java/com/example/shoespricecomparision/MainActivity.java
+++ app/src/main/java/com/example/shoespricecomparision/MainActivity.java
@@ -1,13 +1,94 @@
 package com.example.shoespricecomparision;
 
+import android.app.FragmentTransaction;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import fragment.FavoriteFragment;
+import fragment.SearchFragment;
+import fragment.SettingFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mBottomNavigationView;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+//    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        mBottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        setupBottomNavigation();
+
+        if (savedInstanceState == null) {
+
+            loadSearchFragment();
+        }
+
     }
+
+    private void setupBottomNavigation() {
+
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        loadSearchFragment();
+                        return true;
+                    case R.id.action_profile:
+                        loadFavoriteFragment();
+                        return true;
+                    case R.id.action_settings:
+                        loadSettingsFragment();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void loadSearchFragment() {
+
+        SearchFragment searchFragment = new SearchFragment();
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.replace(R.id.fragment_frame,searchFragment);
+        fragmentTransaction.commit();
+    }
+
+    private void loadFavoriteFragment() {
+        FavoriteFragment favoriteFragment = new FavoriteFragment();
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.replace(R.id.fragment_frame,favoriteFragment);
+        fragmentTransaction.commit();
+
+    }
+
+    private void loadSettingsFragment() {
+
+
+        SettingFragment settingFragment = new SettingFragment();
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.replace(R.id.fragment_frame,settingFragment);
+        fragmentTransaction.commit();
+
+    }
+
 }
