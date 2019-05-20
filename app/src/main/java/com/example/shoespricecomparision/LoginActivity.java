88865b0b443a81ad6158fbf0c3f38dbecde87f36
ashish ghimire <ashish.gh123@gmail.com>
2019-05-20 23:09:48 +0545

    Add login activity and xml

--- app/src/main/java/com/example/shoespricecomparision/LoginActivity.java
+++ app/src/main/java/com/example/shoespricecomparision/LoginActivity.java
@@ -1,13 +1,45 @@
 package com.example.shoespricecomparision;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button btnLogin;
+    private Button btnForgotPassword;
+    private Button btnSignUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etEmail = findViewById(R.id.etEmailLogin);
+        etPassword = findViewById(R.id.etPasswordLogin);
+        btnLogin= findViewById(R.id.btnLogin);
+        btnForgotPassword= findViewById(R.id.btn_reset_password);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
