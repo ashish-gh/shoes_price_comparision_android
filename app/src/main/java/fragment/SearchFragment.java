40b0e75e16848b51f958b466b16e205a229cf0c7
ashish ghimire <ashish.gh123@gmail.com>
2019-05-27 23:18:19 +0545

    Add shoes description activity

--- app/src/main/java/fragment/SearchFragment.java
+++ app/src/main/java/fragment/SearchFragment.java
@@ -3,6 +3,8 @@ package fragment;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,7 @@ import com.example.shoespricecomparision.R;
  */
 public class SearchFragment extends Fragment {
 
+    private RecyclerView recyclerView;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -25,6 +28,7 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
-    }
+
+         }
 
 }
