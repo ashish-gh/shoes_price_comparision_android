135fe77ef751943312ab5e5858aafba018fed7be
ashish ghimire <ashish.gh123@gmail.com>
2019-06-09 08:12:36 +0545

    Add Logout

--- app/src/main/java/fragment/SearchFragment.java
+++ app/src/main/java/fragment/SearchFragment.java
@@ -8,6 +8,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.shoespricecomparision.R;
@@ -31,6 +34,10 @@ public class SearchFragment extends Fragment {
 
     private RecyclerView recyclerViewListShoes;
 
+    private Spinner spinner;
+
+    String[] bankNames={"BOI","SBI","HDFC","PNB","OBC"};
+
     public SearchFragment() {
         // Required empty public constructor
     }
@@ -42,6 +49,22 @@ public class SearchFragment extends Fragment {
         // Inflate the shoes_admin for this fragment
         View view = inflater.inflate(R.layout.fragment_search,container,false);
         recyclerViewListShoes = (RecyclerView) view.findViewById(R.id.recyclerViewListShoes);
+        spinner = view.findViewById(R.id.spinner1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item,bankNames);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String bank_name = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getActivity(), bank_name, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         ShoesAPI shoesAPI = Url.getInstance().create(ShoesAPI.class);
 
